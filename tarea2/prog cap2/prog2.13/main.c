ada6d10cbd29ba2dad71b0572dac7f9fc37b8b1b
jean <ejemplo@gmail.com>
2026-09-19 14:33:28 -0400

    prog cap2

---
+++ tarea2/prog cap2/prog2.13/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+  float X;
+  int Y;
+  printf("\nIngrese el valor de Y: ");
+  scanf("%d", &Y);
+  if (Y < 0 || Y > 50)
+  {
+      X = 0;
+  }else
+  {
+      if (Y <= 10)
+      {
+          X = 4 / Y - Y;
+      }else
+      {
+           if (Y <= 25)
+      {
+          X = pow(Y, 3) - 12;
+      }else
+      {
+          X = pow(Y, 2) + pow(Y, 3) - 18;
+       }
+      }
+
+
+  }
+  printf("\n\mY = %d\tX = %8.2f", Y, X);
+}
